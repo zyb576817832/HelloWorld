7840c2c90e4d9f2aa4811fab2909c7a97f4def36
xiweiwei <1269832742@qq.com>
2016-06-29 11:17:01 +0800

    zyb是个渣渣

---
+++ HelloWorld/study/src/com/zyb/Hello.java
@@ -0,0 +1,12 @@
+package com.zyb;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello world,sb yanbio");
+		System.out.println("这真是一个令人痛心的代码");
+			System.out.println("写的贼炸");
+
+	}
+}
